51e2f469d9412616a8195b40c0b3e49043749d27
sami <samialabdalah@yahoo.com>
2023-04-08 18:31:32 +0200

    Working with diffrent classes

--- CaseManagementSystem/Services/MenuCustomerServiceEmployee.cs
+++ CaseManagementSystem/Services/MenuCustomerServiceEmployee.cs
@@ -43,7 +43,7 @@ internal class MenuCustomerServiceEmployee
             else
             {
                 Console.Clear();
-                Console.WriteLine($"\n -Inget ärende träffat med den ärendenummer: {id} .");
+                Console.WriteLine($"\n -Inget ärende träffat med det ärendenummer: {id}.");
                 Console.WriteLine("");
             }
         }
@@ -92,47 +92,23 @@ internal class MenuCustomerServiceEmployee
         }
     }
 
-    public async Task DeleteSpecificSituationAsync()
-    {
-        Console.Write("Ange det ID-ärende som vill du ha att ta bort: ");
-        var id = Console.ReadLine();
-        if (!String.IsNullOrEmpty(id))
-        {
-            bool deleted = await CustomerServiceEmployee.DeleteAsync(int.Parse(id));
-
-            if (deleted)
-            {
-                Console.WriteLine("Ärende raderat");
-            }
-            else
-            {
-                Console.WriteLine("Kunde inte att hitta något ärende med den ID.");
-            }
-        }
-        else
-        {
-            Console.WriteLine($"Ange ärende-ID.");
-            Console.WriteLine("");
-        }
-    }
-
     public async Task AddCommentToSituationAsync()
     {
-        Console.Write("Ange ID-ärende du vill kommentera: ");
+        Console.Write("\n - Ange ärendenummer du vill kommentera: ");
         int situationId = int.Parse(Console.ReadLine() ?? "");
 
-        Console.WriteLine("Vi behöver din information för vet vem som följer ärende");
-        Console.WriteLine("Kundtjänstmedarbetare-information:");
-        Console.Write("Förnamn: ");
+        Console.WriteLine("\n- Vi behöver din information för vet vem som följer ärende!");
+        Console.WriteLine("\n - Kundtjänstmedarbetare-information:");
+        Console.Write("\n - Förnamn: ");
         string firstName = Console.ReadLine() ?? "";
 
-        Console.Write("Efternamn: ");
+        Console.Write("\n - Efternamn: ");
         string lastName = Console.ReadLine() ?? "";
 
-        Console.Write("E-post: ");
+        Console.Write("\n - E-post: ");
         string email = Console.ReadLine() ?? "";
 
-        Console.Write("Telefonnummer: ");
+        Console.Write("\n - Telefonnummer: ");
         string phoneNumber = Console.ReadLine() ?? "";
 
         Models.CustomerServiceEmployees customerServiceEmployee = new Models.CustomerServiceEmployees
@@ -143,7 +119,7 @@ internal class MenuCustomerServiceEmployee
             PhoneNumber = phoneNumber
         };
 
-        Console.Write("Skriv in kommentarstexten: ");
+        Console.Write("\n - Skriv in kommentarstexten: ");
         string commentText = Console.ReadLine() ?? "";
 
         Comments comment = new Comments
@@ -154,28 +130,28 @@ internal class MenuCustomerServiceEmployee
 
         await CustomerServiceEmployee.AddCommentAsync(situationId, comment, customerServiceEmployee);
 
-        Console.WriteLine("Kommentaren har sparat.");
+        Console.WriteLine("\n +++++++ Kommentaren har sparat! +++++++++");
     }
 
     public async Task ShowCommentToSituationAsync()
     {
-        Console.Write("Ange ärende-ID: ");
+        Console.Write("\n - Ange ärendenummer : ");
         int situationId = int.Parse(Console.ReadLine() ?? "");
         var existingSituation = await CustomerServiceEmployee.GetAsync(situationId);
 
         if (existingSituation != null)
         {
-            Console.WriteLine($"Beskrivning: {existingSituation.Description}");
-            Console.WriteLine($"SkapadTid: {existingSituation.CreatedTime}");
-            Console.WriteLine($"Situation: {existingSituation.Condition}");
-            Console.WriteLine($"Kund namn: {existingSituation.FirstName} {existingSituation.LastName}");
-            Console.WriteLine($"Kund e-post: {existingSituation.Email}");
-            Console.WriteLine($"Kund Telefonnummer: {existingSituation.PhoneNumber}");
+            Console.WriteLine("\n############################################################");
+            Console.WriteLine($"\n- Beskrivning: {existingSituation.Description}");
+            Console.WriteLine($"- Situation och AnmälaTid: {existingSituation.Condition + " - " + existingSituation.CreatedTime}");
+            Console.WriteLine($"- Kund namn: {existingSituation.FirstName + " " + existingSituation.LastName}");
+            Console.WriteLine($"- Kund-info: {existingSituation.Email + " ; tel: " + existingSituation.PhoneNumber}\n");
+            Console.WriteLine("############################################################");
             Console.WriteLine();
         }
         if (existingSituation == null)
         {
-            Console.WriteLine($"Inget ärende hittades med ID {situationId}");
+            Console.WriteLine($"\n -Inget ärende träffat med det ärendenummer: {situationId}.");
             return;
         }
         var situationWithComments = await CustomerServiceEmployee.GetCommentsAsync(situationId);
@@ -184,30 +160,30 @@ internal class MenuCustomerServiceEmployee
         {
             foreach (var comment in situationWithComments)
             {
-                Console.WriteLine($"Kundtjänstmedarbetare kommentar: {comment.Text} (posted on {comment.CreatedAt})");
+                Console.WriteLine($" + Kundtjänstmedarbetare kommentar: {comment.Text} ( {comment.CreatedAt} ).");
             }
 
             Console.WriteLine();
         }
         else
         {
-            Console.WriteLine("Ingen kommentar i det ärende");
-            Console.WriteLine("Vill du ha att lägga ny kommentar, JA eller NEJ");
+            Console.WriteLine(" - Ingen kommentar i det ärende\n");
+            Console.WriteLine(" + Vill du ha att lägga ny kommentar, JA eller NEJ : ");
             var option = Console.ReadLine() ?? "";
             if (option == "ja".ToLower())
             {
-                Console.WriteLine("Vi behöver din information för vet vem som följer ärende");
-                Console.WriteLine("Kundtjänstmedarbetare-information:");
-                Console.Write("Förnamn: ");
+                Console.WriteLine("\n - Vi behöver din information för vet vem som följer ärende!");
+                Console.WriteLine(" - Kundtjänstmedarbetare-information:");
+                Console.Write("\n - Förnamn: ");
                 string firstName = Console.ReadLine() ?? "";
 
-                Console.Write("Efternamn: ");
+                Console.Write(" - Efternamn: ");
                 string lastName = Console.ReadLine() ?? "";
 
-                Console.Write("E-post: ");
+                Console.Write(" - E-post: ");
                 string email = Console.ReadLine() ?? "";
 
-                Console.Write("Telefonnummer: ");
+                Console.Write(" - Telefonnummer: ");
                 string phoneNumber = Console.ReadLine() ?? "";
                 Models.CustomerServiceEmployees customerServiceEmployee = new Models.CustomerServiceEmployees
                 {
@@ -217,7 +193,7 @@ internal class MenuCustomerServiceEmployee
                     PhoneNumber = phoneNumber
                 };
 
-                Console.Write("Skriv din kommentar: ");
+                Console.Write("\n + Skriv din kommentar: ");
                 string commentText = Console.ReadLine() ?? "";
 
                 Comments _comment = new()
@@ -228,12 +204,36 @@ internal class MenuCustomerServiceEmployee
 
                 await CustomerServiceEmployee.AddCommentAsync(situationId, _comment, customerServiceEmployee);
 
-                Console.WriteLine("Kommentaren har sparat.");
+                Console.WriteLine("\n + Kommentaren har sparat.");
             }
             else
             {
-                Console.WriteLine("Välkommen åter, när vill att lägga en kommentar.");
+                Console.WriteLine("\n - Välkommen åter, när vill att lägga en kommentar.");
             }
+        }
+    }
+
+    public async Task DeleteSpecificSituationAsync()
+    {
+        Console.Write("\n - Ange det ärendenummer som vill du ha att ta bort: ");
+        var id = Console.ReadLine();
+        if (!String.IsNullOrEmpty(id))
+        {
+            bool deleted = await CustomerServiceEmployee.DeleteAsync(int.Parse(id));
+
+            if (deleted)
+            {
+                Console.WriteLine("\n -Ärende raderat!");
+            }
+            else
+            {
+                Console.WriteLine("\n - Kunde inte att hitta något ärende med det ärendenummer.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"\n - Ange ärendenummer.");
+            Console.WriteLine("");
         }
     }
 }
